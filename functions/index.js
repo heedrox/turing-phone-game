ef97440c4a31e972c14ed360d014f500f74f7586
Jordi Marti <heedrox@gmail.com>
2022-08-20 18:48:10 +0200

    create endpoint for openai

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
 const functions = require("firebase-functions");
+const { Configuration, OpenAIApi } = require("openai");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +8,28 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const PROMPT_TEMPLATE = (question) => `The following is a conversation with an AI assistant. The assistant is joyful, clever and very friendly, and always answers with one sentence. Human: ${question} IA: `
+
+exports.aiTest = functions.https.onRequest(async (request, response) => {
+    const question = request.query.question
+
+    if (!question) {
+        response.status(400).send({ error: { code: 400, description: "Please, add a question parameter." }})
+        return
+    }
+    const completion = await openai.createCompletion({
+        model: "text-curie-001",
+        prompt: PROMPT_TEMPLATE(question),
+        temperature: 0,
+        max_tokens: 200,
+    });
+
+    response.send(completion.data.choices[0].text)
+})
