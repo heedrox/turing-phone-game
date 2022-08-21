86d8e95c02ab64a1965219aa1c388d40998530a9
Jordi Marti <heedrox@gmail.com>
2022-08-21 15:24:01 +0200

    add eslint

--- turing-phone-web/src/router/index.js
+++ turing-phone-web/src/router/index.js
@@ -1,15 +1,17 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../pages/StartGame.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import StartGame from '../pages/StartGame.vue';
+import Home from '../pages/Home.vue';
 
 const routes = [
-    { path: "/", component: Home },
+  { path: '/', component: Home },
+  { path: '/game/:gameId/:numberOfPlayers/player/:numPlayer/:namePlayer/start', component: StartGame },
 ];
 
 const history = createWebHistory();
 
 const router = createRouter({
-    history,
-    routes,
+  history,
+  routes,
 });
 
 export default router;
